6b3554a03a636bb1b7c037245809bd5dde52027e
na5368s <ME683uxgp>
2018-03-08 16:46:52 +0100

    Refactoring Expdp.
    Reading tnsnames.ora when it is set in system variable as 'TNS_NAMES'.

--- src/app/connection.ts
+++ src/app/connection.ts
@@ -1,31 +1,69 @@
+import {Observable} from 'rxjs/Observable';
+import {fromPromise} from 'rxjs/observable/fromPromise';
+import {catchError, tap} from 'rxjs/operators';
+import {ChildProcess} from 'child_process';
+
+const _oracledb = (<any>window).require('oracledb');
+
 const _knex = (<any>window).require('knex');
+
+const {spawn} = (<any>window).require('child_process');
 
 export class Connection {
 
-  isConnected = false;
-  constructor(
-    public username: string,
-    public password: string,
-    public db: string,
-    public extusername: string,
-    public dump: string
-  ) { }
+    tnsPath;
 
-  checkConnection() {
-    const knex = _knex({
-      client: 'oracledb',
-      connection: {
-        host: 'localhost',
-        user: this.username,
-        password: this.password,
-        database: this.db,
+    constructor(public username: string,
+                public password: string,
+                public db: string,
+                public extusername: string,
+                public dump: string) {
+    }
 
-      }
-    });
-    this.isConnected = true;
-    knex.raw('select * from Test').catch(err => {
-      this.isConnected = false;
-    });
-  }
+    setTnsNames(): Observable<ChildProcess> {
+        const commandLine = `echo %TNS_NAMES%`;
+        const bat: ChildProcess = spawn('cmd.exe', ['/c', commandLine]);
+
+        return new Observable(obs => {
+            bat.stdout.on('data', (data) => {
+                this.tnsPath = data.toString() + '\\tnsnames.ora';
+                this.tnsPath = this.tnsPath.replace(/(\r\n|\n|\r)/gm, '');
+                console.log(this.tnsPath);
+            });
+
+            bat.stderr.on('data', (data) => {
+                console.log(data.toString());
+            });
+
+            bat.on('exit', (code) => {
+                console.log('Child exited with code ${code}');
+                obs.next(bat);
+                obs.complete();
+            });
+
+        });
+    }
+
+    checkConnection(): Observable<any> {
+        const knex = _knex({
+            client: 'oracledb',
+            connection: {
+                host: 'srv-db-fls',
+                user: this.username,
+                password: this.password,
+                database: 'FLSKDDB.FLS.DE',
+
+            }
+        });
+
+        return fromPromise(knex.raw('select sysdate from dual'))
+            .pipe(
+                catchError(err => {
+                    console.log(err);
+                    return Observable.throw(err);
+                }),
+                tap(console.log)
+            );
+    }
 
 }
--- src/app/dump-export/dump-export.component.ts
+++ src/app/dump-export/dump-export.component.ts
@@ -1,73 +1,81 @@
-import { Component, OnInit } from '@angular/core';
-import { Connection } from '../connection';
-import { Observable } from 'rxjs/Observable';
+import {Component, OnInit} from '@angular/core';
+import {Connection} from '../connection';
+import {Observable} from 'rxjs/Observable';
+
 import 'rxjs/add/observable/of';
+import {bindCallback} from 'rxjs/observable/bindCallback';
+import {ChildProcess} from 'child_process';
+import {concatMap} from 'rxjs/operators';
+
 const _knex = (<any>window).require('knex');
 const _oracledb = (<any>window).require('oracledb');
 const _execa = (<any>window).require('execa');
 const _fs = (<any>window).require('fs');
 const cmd = (<any>window).require('node-cmd');
+const {spawn} = (<any>window).require('child_process');
 
 
 @Component({
-  selector: 'app-dump-export',
-  templateUrl: './dump-export.component.html',
-  styleUrls: ['./dump-export.component.css']
+    selector: 'app-dump-export',
+    templateUrl: './dump-export.component.html',
+    styleUrls: ['./dump-export.component.css']
 })
 export class DumpExportComponent implements OnInit {
 
-  model = new Connection('', '', '', '', '');
+    model = new Connection('ly', 'ly', 'flskddb', 'a', 'ly');
 
-  submitted = false;
-  connecting = false;
-  error = false;
-  success = false;
+    submitted = false;
 
-  onSubmit() {
-    this.error = false;
-    this.submitted = true;
-    this.connecting = true;
-    this.success = false;
-    this.model.checkConnection();
-    setTimeout(() => {
-      if (this.model.isConnected) {
-        this.connecting = false;
-        this.dumpexport();
-      } else {
-        this.error = true;
-        this.connecting = false;
-      }
-    }, 1000);
-  }
+    onSubmit() {
+        this.submitted = true;
+        this.model.setTnsNames().subscribe(res => {
+            console.log('tns-success: ', res);
+            _fs.readFile(this.model.tnsPath, 'utf8', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(data);
+            });
+        }, res => {
+            console.log('tns-error', res);
+        });
+        this.model.checkConnection()
+            .pipe(concatMap(() => this.dumpexport()))
+            .subscribe(res => {
+                console.log('submitted', res);
+                this.submitted = false;
+            }, res => {
+                this.submitted = false;
+                console.log('Verbindung fehlgeschlagen:' + res);
+            });
+    }
 
-  dumpexport() {
-    const myObservable = Observable.of();
-    const myObserver1 = {
-       complete: () => this.submitted = false,
-     };
-    const myObserver2 = {
-      complete: () => this.error = true,
-    };
-    const myObserver3 = {
-      complete: () => this.success = true,
-    };
-    const commandline = 'expdp ' + this.model.username + '/' + this.model.password + '@' + this.model.db + ' DUMPFILE=' + this.model.dump;
-    cmd.get(commandline, function(err, data, stderr) {
-        if (!err) {
-            myObservable.subscribe(myObserver1);
-            myObservable.subscribe(myObserver3);
-        } else {
-            console.log(err);
-            myObservable.subscribe(myObserver1);
-            myObservable.subscribe(myObserver2);
-        }
-    });
+    dumpexport(): Observable<ChildProcess> {
+        const commandLine = `expdp ${this.model.username}/${this.model.password}@${this.model.db} DUMPFILE=${this.model.dump}`;
+        const bat: ChildProcess = spawn('cmd.exe', ['/c', commandLine]);
 
-  }
+        return new Observable(obs => {
+            bat.stdout.on('data', (data) => {
+                console.log(data.toString());
+            });
 
-  constructor() { }
+            bat.stderr.on('data', (data) => {
+                console.log(data.toString());
+            });
 
-  ngOnInit() {
-  }
+            bat.on('exit', (code) => {
+                console.log('Child exited with code ${code}');
+                obs.next(bat);
+                obs.complete();
+            });
+
+        });
+    }
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
